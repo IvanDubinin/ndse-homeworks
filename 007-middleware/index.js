241229ffc6bca422bac0b2de157bd6a6b7a94128
Ivan Dubinin <ivan.a.dubinin@gmail.com>
2022-09-03 22:41:39 +0300

    Update index.js

--- 007-middleware/index.js
+++ 007-middleware/index.js
@@ -1,23 +1,10 @@
 const express = require("express");
-
-const getAllBooksRouter = require("./routes/getAllBooksRouter");
-const getBookByIdRouter = require("./routes/getBookByIdRouter");
-const createBookRouter = require("./routes/createBookRouter");
-const userLoginRouter = require("./routes/userLoginRouter");
-const updateBookByIdRouter = require("./routes/updateBookByIdRouter");
-const deleteBookByIdRouter = require("./routes/deleteBookByIdRouter");
-const getFileBookByIdRouter = require("./routes/getFileBookByIdRouter");
+const booksRouter = require("./routes/booksRouter");
 
 const app = express();
 app.use(express.json());
 
-app.use(getAllBooksRouter);
-app.use(getBookByIdRouter);
-app.use(createBookRouter);
-app.use(userLoginRouter);
-app.use(updateBookByIdRouter);
-app.use(deleteBookByIdRouter);
-app.use(getFileBookByIdRouter);
+app.use("/api/books", booksRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
