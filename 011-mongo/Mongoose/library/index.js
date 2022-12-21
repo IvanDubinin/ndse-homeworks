aab74ec1104caec544d55fa64fe91c38be3accc5
Ivan Dubinin <ivan.a.dubinin@gmail.com>
2022-12-21 16:59:48 +0300

    Update index.js

--- 011-mongo/Mongoose/library/index.js
+++ 011-mongo/Mongoose/library/index.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const booksRouter = require("./routes/booksRouter");
+const mongoose = require("mongoose");
 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-
 app.use("/api/books", booksRouter);
 
+const UrlDB = process.env.UrlDB;
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log("Library app is listening on http://localhost:3000/api/books");
-  });
+
+async function start(PORT, UrlDB) {
+  try {
+    await mongoose.connect(UrlDB);
+    app.listen(PORT, () => {
+      console.log("Library app is listening on http://localhost:3000/api/books");
+    });
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+start(PORT, UrlDB);
